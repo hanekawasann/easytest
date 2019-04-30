ff9beb4b6207222d0748067dd07b2f5bb3b70aca
yukms <763803382@qq.com>
2019-04-30 16:16:07 +0800

    初始化项目

---
+++ src/main/java/com/yukms/easy/test/EasytestApplication.java
@@ -0,0 +1,13 @@
+package com.yukms.easy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasytestApplication.class, args);
+    }
+
+}
